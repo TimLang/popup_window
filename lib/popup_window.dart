b4bf8c6af9b58c1375c9766158930f0cdb5ebdc3
TimLang <langyong135@gmail.com>
2021-12-31 15:26:53 +0800

    fix offset

--- lib/popup_window.dart
+++ lib/popup_window.dart
@@ -81,7 +81,7 @@ class _PopupWindowButtonState<T> extends State<PopupWindowButton> {
     final RenderBox overlay = Overlay.of(context).context.findRenderObject();
     final RelativeRect position = RelativeRect.fromRect(
       Rect.fromPoints(
-        button.localToGlobal(widget.offset, ancestor: overlay),
+        button.localToGlobal(offset, ancestor: overlay),
         button.localToGlobal(button.size.bottomRight(Offset.zero),
             ancestor: overlay),
       ),
